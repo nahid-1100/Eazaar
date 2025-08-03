8d04f146a0bdd99a85d60edf1403785df62f7edb
nahid-1100 <nahid-1100>
2025-08-03 10:45:05 +0600

    product data feching and hook create for data loading with tanstack query

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,5 +1,21 @@
-const products = () => {
-  // console.log()
+import { useQuery } from "@tanstack/react-query";
+
+const useProducts = () => {
+  // Access the client
+
+  const {
+    data = [],
+    isPending,
+    isError,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => {
+      fetch("./product.json").then((res) => res.json());
+    },
+  });
+  return { data, isPending, isError, error, refetch };
 };
 
-export default products;
+export default useProducts;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./Router/Router";
 import "./assets/styles/sections.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Create a client
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="container mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   </StrictMode>
 );
